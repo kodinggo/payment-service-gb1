4b49e4f16725fcfb4661d703ee083b56fbe3a535
Tubagus M Fadillah <tubagusfadillah98@gmail.com>
2024-08-26 21:17:36 +0700

    add file go.mod, go.sum, handler, etc

---
+++ internal/console/root.go
@@ -0,0 +1,27 @@
+package console
+
+import (
+	"os"
+
+	"github.com/tubagusmf/payment-service-gb1/internal/config"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCMd = &cobra.Command{
+	Use:   "console",
+	Short: "Console commands",
+	Long:  "Console commands",
+}
+
+func Execute() {
+	err := rootCMd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCMd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	config.SetupLogger()
+}
